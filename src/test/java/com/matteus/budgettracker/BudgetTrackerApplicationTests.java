d231c70e692c9a3f62bbd5f1ab6e37bcef383be8
unknown <matteus.vinasco@stud.sti.se>
2024-11-23 12:39:02 +0100

    first commit, updating properties

---
+++ src/test/java/com/matteus/budgettracker/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matteus.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
