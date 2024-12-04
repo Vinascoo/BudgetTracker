2850cd285b305d3ab97d17bc422720291e24461d
unknown <matteus.vinasco@stud.sti.se>
2024-12-04 20:11:53 +0100

    minorChanges

--- src/main/java/com/matteus/budgettracker/config/CorsConfig.java
+++ src/main/java/com/matteus/budgettracker/config/CorsConfig.java
@@ -1,2 +1,22 @@
-package com.matteus.budgettracker.config;public class CorsConfig {
+package com.matteus.budgettracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:19006") // Din React Native app's utvecklings-URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
